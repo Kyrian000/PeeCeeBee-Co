29d5efe604b996937696b9f8f2681b6843c0be80
Chibueze Okafor-onyili <Kyrian000>
2024-12-02 09:38:55 +0100

    Testing About navbar

---
+++ about.js
@@ -0,0 +1,24 @@
+// Solidify navbar on scroll
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
+
+
+
+const menu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    menu.style.display = "flex"
+    menuBtn.style.display = "none";
+    closeBtn.style.display = "inline-block";
+});
+
+closeBtn.addEventListener('click', () => {
+    menu.style.display = "none"
+    menuBtn.style.display = "inline-block";
+    closeBtn.style.display = "none";
+
+});
